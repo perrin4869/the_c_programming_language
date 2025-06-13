9113decf06b89c9d4bd6ff8c1758db922312966c
Julian Grinblat <julian@dotcore.co.il>
2025-06-13 13:33:58 +0900

    ch06: countwords

---
+++ ch06/tnode.c
@@ -0,0 +1,50 @@
+#include "tnode.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct tnode *talloc(void);
+char *strdup(char *);
+
+/* addtree: add a node with w, at or below p */
+struct tnode *addtree(struct tnode *p, char *w) {
+  int cond;
+
+  if (p == NULL) { /* a new word has arrived */
+    p = talloc();  /* make a new node */
+    p->word = strdup(w);
+    p->count = 1;
+    p->left = p->right = NULL;
+  } else if ((cond = strcmp(w, p->word)) == 0)
+    p->count++;      /* repeated word */
+  else if (cond < 0) /* less than into left subtree */
+    p->left = addtree(p->left, w);
+  else /* greater than into right subtree */
+    p->right = addtree(p->right, w);
+  return p;
+}
+
+/* treeprint: in-order print of tree p */
+void treeprint(struct tnode *p) {
+  if (p != NULL) {
+    treeprint(p->left);
+    printf("%4d %s\n", p->count, p->word);
+    treeprint(p->right);
+  }
+}
+
+/* talloc: make a tnode */
+struct tnode *talloc(void) {
+  return (struct tnode *)malloc(sizeof(struct tnode));
+}
+
+char *strdup(char *s) /* make a duplicate of s */
+{
+  char *p;
+
+  p = (char *)malloc(strlen(s) + 1); /* +1 for '\0' */
+  if (p != NULL)
+    strcpy(p, s);
+  return p;
+}
